3806257362da2bf055789df2d6bec0de3eee2d86
EC2 Default User <ec2-user@ip-172-31-38-222.us-west-2.compute.internal>
2018-01-15 13:24:11 +0000

    Fix hardcoded URL in correct dir this time

--- ubuntu/invoke-phantom.js
+++ ubuntu/invoke-phantom.js
@@ -1,5 +1,5 @@
 var webpage = require('webpage');
 var page = webpage.create()
-page.open('http://www.ibm.com:8080/green/timer/dots', function(status) {
+page.open('http://CHANGEME:8080/green/timer/dots', function(status) {
   page.close();
 });
