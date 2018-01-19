4339aeaa3716d4fc132b4aab0040e6c9149d2ad9
EC2 Default User <ec2-user@ip-172-31-6-54.us-west-2.compute.internal>
2018-01-19 13:33:13 +0000

    Hardcode gocontainer status

--- ubuntu/invoke-phantom.js
+++ ubuntu/invoke-phantom.js
@@ -1,5 +1,5 @@
 var webpage = require('webpage');
 var page = webpage.create()
-page.open('http://CHANGEME:8080/green/timer/dots', function(status) {
+page.open('http://gocontainer.redf4rth.net:8080/green/timer/dots', function(status) {
   page.close();
 });
